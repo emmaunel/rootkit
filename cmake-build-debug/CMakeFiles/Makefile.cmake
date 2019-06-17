dc5093d56da9eef467418862d3383bd1add1b9a2
Emmanuel <aadewale123@gmail.com>
2019-06-17 19:50:59 -0400

    config

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -45,7 +45,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/rootkit.dir/DependInfo.cmake"
   "CMakeFiles/client.dir/DependInfo.cmake"
   "CMakeFiles/config.dir/DependInfo.cmake"
   )
